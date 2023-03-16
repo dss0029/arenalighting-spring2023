cd44bdd0eddd5296be9b021f30c7f010868b756e
Devin Spivey <56698580+dss0029@users.noreply.github.com>
2023-03-16 14:11:51 -0500

    Added color cross-fading (#7)
    Added UI option to switch between instant and smooth color transitions. Smooth transitions will iterate over a gradient color spectrum between the start and end color.

--- stadium/Assets/Scripts/Color Control/ButtonController.cs
+++ stadium/Assets/Scripts/Color Control/ButtonController.cs
@@ -16,14 +16,47 @@ public class ButtonController : MonoBehaviour
     public Button auBlue2;
     public Button auOrange1;
     public Button auOrange2;
+    public GameObject LEDTemplate;
+    public Toggle fadeToggle;
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
+    public bool fading;
+    public Color fadeEnd;
+    public float fadeFrame;
 
     void Start()
     {
+        fadeFrame = 0.0f;
         //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
         auBlue1.onClick.AddListener(SetBlue1);
         auBlue2.onClick.AddListener(SetBlue2);
         auOrange1.onClick.AddListener(SetOrange1);
         auOrange2.onClick.AddListener(SetOrange2);
+    }
+
+    void Update()
+    {
+        string tag;
+        GameObject[] allLEDs;
+        tag = "LED";
+        allLEDs = GameObject.FindGameObjectsWithTag(tag);
+        if (fading)
+        {
+            fadeFrame = fadeFrame + 0.1f;
+            Color frameColor = gradient.Evaluate(fadeFrame);
+            foreach (GameObject LED in allLEDs)
+            {
+                LED.GetComponent<Renderer>().material.color = frameColor;
+                LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", frameColor);
+            }
+            Color currentColor = LEDTemplate.GetComponent<Renderer>().material.color;
+            if (currentColor == fadeEnd)
+            {
+                fading = false;
+                Debug.Log("Done");
+            }
+        }
     }
 
     void SetBlue1()
@@ -34,11 +67,38 @@ public class ButtonController : MonoBehaviour
         GameObject[] allLEDs;
         tag = "LED";
         allLEDs = GameObject.FindGameObjectsWithTag(tag);
-        newCol = new Color(0.01176471f, 0.1411765f, 0.3019608f, .5f);
-        foreach (GameObject LED in allLEDs)
+        newCol = new Color(0.012f, 0.141f, 0.302f, .500f);
+        if (!fadeToggle.isOn)
         {
-            LED.GetComponent<Renderer>().material.color = newCol;
-            LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+         
+            foreach (GameObject LED in allLEDs)
+            {
+                LED.GetComponent<Renderer>().material.color = newCol;
+                LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+            }
+           
+        }
+        else {
+            Color startCol = LEDTemplate.GetComponent<Renderer>().material.color;
+            Color endCol = newCol;
+            gradient = new Gradient();
+
+            colorKey = new GradientColorKey[2];
+            colorKey[0].color = startCol;
+            colorKey[0].time = 0.0f;
+            colorKey[1].color = endCol;
+            colorKey[1].time = 1.0f;
+
+            alphaKey = new GradientAlphaKey[2];
+            alphaKey[0].alpha = 0.5f;
+            alphaKey[0].time = 0.0f;
+            alphaKey[1].alpha = 0.5f;
+            alphaKey[1].time = 1.0f;
+
+            gradient.SetKeys(colorKey, alphaKey);
+            fadeEnd = endCol;
+            fadeFrame = 0.0f;
+            fading = true;
         }
     }
 
@@ -51,10 +111,36 @@ public class ButtonController : MonoBehaviour
         tag = "LED";
         allLEDs = GameObject.FindGameObjectsWithTag(tag);
         newCol = new Color(0.2862745f, 0.4313726f, 0.6117647f, .5f);
-        foreach (GameObject LED in allLEDs)
+        if (!fadeToggle.isOn)
         {
-            LED.GetComponent<Renderer>().material.color = newCol;
-            LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+            foreach (GameObject LED in allLEDs)
+            {
+                LED.GetComponent<Renderer>().material.color = newCol;
+                LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+            }
+        }
+        else
+        {
+            Color startCol = LEDTemplate.GetComponent<Renderer>().material.color;
+            Color endCol = newCol;
+            gradient = new Gradient();
+
+            colorKey = new GradientColorKey[2];
+            colorKey[0].color = startCol;
+            colorKey[0].time = 0.0f;
+            colorKey[1].color = endCol;
+            colorKey[1].time = 1.0f;
+
+            alphaKey = new GradientAlphaKey[2];
+            alphaKey[0].alpha = 0.5f;
+            alphaKey[0].time = 0.0f;
+            alphaKey[1].alpha = 0.5f;
+            alphaKey[1].time = 1.0f;
+
+            gradient.SetKeys(colorKey, alphaKey);
+            fadeEnd = endCol;
+            fadeFrame = 0.0f;
+            fading = true;
         }
     }
 
@@ -67,10 +153,36 @@ public class ButtonController : MonoBehaviour
         tag = "LED";
         allLEDs = GameObject.FindGameObjectsWithTag(tag);
         newCol = new Color(0.8666667f, 0.3333333f, 0.04705882f, .5f);
-        foreach (GameObject LED in allLEDs)
+        if (!fadeToggle.isOn)
         {
-            LED.GetComponent<Renderer>().material.color = newCol;
-            LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+            foreach (GameObject LED in allLEDs)
+            {
+                LED.GetComponent<Renderer>().material.color = newCol;
+                LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+            }
+        }
+        else
+        {
+            Color startCol = LEDTemplate.GetComponent<Renderer>().material.color;
+            Color endCol = newCol;
+            gradient = new Gradient();
+
+            colorKey = new GradientColorKey[2];
+            colorKey[0].color = startCol;
+            colorKey[0].time = 0.0f;
+            colorKey[1].color = endCol;
+            colorKey[1].time = 1.0f;
+
+            alphaKey = new GradientAlphaKey[2];
+            alphaKey[0].alpha = 0.5f;
+            alphaKey[0].time = 0.0f;
+            alphaKey[1].alpha = 0.5f;
+            alphaKey[1].time = 1.0f;
+
+            gradient.SetKeys(colorKey, alphaKey);
+            fadeEnd = endCol;
+            fadeFrame = 0.0f;
+            fading = true;
         }
     }
 
@@ -83,10 +195,36 @@ public class ButtonController : MonoBehaviour
         tag = "LED";
         allLEDs = GameObject.FindGameObjectsWithTag(tag);
         newCol = new Color(0.9647059f, 0.5019608f, 0.1490196f, .5f);
-        foreach (GameObject LED in allLEDs)
+        if (!fadeToggle.isOn)
         {
-            LED.GetComponent<Renderer>().material.color = newCol;
-            LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+            foreach (GameObject LED in allLEDs)
+            {
+                LED.GetComponent<Renderer>().material.color = newCol;
+                LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+            }
+        }
+        else
+        {
+            Color startCol = LEDTemplate.GetComponent<Renderer>().material.color;
+            Color endCol = newCol;
+            gradient = new Gradient();
+
+            colorKey = new GradientColorKey[2];
+            colorKey[0].color = startCol;
+            colorKey[0].time = 0.0f;
+            colorKey[1].color = endCol;
+            colorKey[1].time = 1.0f;
+
+            alphaKey = new GradientAlphaKey[2];
+            alphaKey[0].alpha = 0.5f;
+            alphaKey[0].time = 0.0f;
+            alphaKey[1].alpha = 0.5f;
+            alphaKey[1].time = 1.0f;
+
+            gradient.SetKeys(colorKey, alphaKey);
+            fadeEnd = endCol;
+            fadeFrame = 0.0f;
+            fading = true;
         }
     }
 
--- stadium/Assets/Scripts/Color Control/InputReader.cs
+++ stadium/Assets/Scripts/Color Control/InputReader.cs
@@ -1,12 +1,95 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InputReader : MonoBehaviour
+public class InputReader : MonoBehaviour 
 {
+    public GameObject LEDTemplate;
+    public Toggle fadeToggle;
+    Gradient gradient;
+    GradientColorKey[] colorKey;
+    GradientAlphaKey[] alphaKey;
+    public bool fading;
+    public Color fadeEnd;
+    public float fadeFrame;
+    public Button submitButton;
+    public Color inputColor;
+    public bool badInput;
+
     void Start()
     {
         var input = gameObject.GetComponent<InputField>();
         input.onEndEdit.AddListener(SubmitName);  // This also works
+        submitButton.onClick.AddListener(buttonPress);
+        badInput = true;
+
+    }
+
+    void Update()
+    {
+        string tag;
+        GameObject[] allLEDs;
+        tag = "LED";
+        allLEDs = GameObject.FindGameObjectsWithTag(tag);
+        if (fading)
+        {
+            fadeFrame = fadeFrame + 0.1f;
+            Color frameColor = gradient.Evaluate(fadeFrame);
+            foreach (GameObject LED in allLEDs)
+            {
+                LED.GetComponent<Renderer>().material.color = frameColor;
+                LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", frameColor);
+            }
+            Color currentColor = LEDTemplate.GetComponent<Renderer>().material.color;
+            if (currentColor == fadeEnd || fadeFrame > 2.0f)
+            {
+                fading = false;
+                Debug.Log("Done");
+            }
+        }
+    }
+
+    void buttonPress()
+    {
+        if (!badInput)
+        {
+            Color newCol;
+            string tag;
+            GameObject[] allLEDs;
+            tag = "LED";
+            allLEDs = GameObject.FindGameObjectsWithTag(tag);
+            newCol = inputColor;
+            if (!fadeToggle.isOn)
+            {
+                foreach (GameObject LED in allLEDs)
+                {
+                    LED.GetComponent<Renderer>().material.color = newCol;
+                    LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
+                }
+            }
+            else
+            {
+                Color startCol = LEDTemplate.GetComponent<Renderer>().material.color;
+                Color endCol = newCol;
+                gradient = new Gradient();
+
+                colorKey = new GradientColorKey[2];
+                colorKey[0].color = startCol;
+                colorKey[0].time = 0.0f;
+                colorKey[1].color = endCol;
+                colorKey[1].time = 1.0f;
+
+                alphaKey = new GradientAlphaKey[2];
+                alphaKey[0].alpha = 0.5f;
+                alphaKey[0].time = 0.0f;
+                alphaKey[1].alpha = 0.5f;
+                alphaKey[1].time = 1.0f;
+
+                gradient.SetKeys(colorKey, alphaKey);
+                fadeEnd = endCol;
+                fadeFrame = 0.0f;
+                fading = true;
+            }
+        }
     }
 
     private void SubmitName(string arg0)
@@ -20,15 +103,13 @@ public class InputReader : MonoBehaviour
 
         if (ColorUtility.TryParseHtmlString(htmlValue, out newCol))
         {
-            foreach (GameObject LED in allLEDs)
-            {
-                LED.GetComponent<Renderer>().material.color = newCol;
-                LED.GetComponent<Renderer>().material.SetColor("_EmissionColor", newCol);
-            }
+            inputColor = newCol;
+            badInput = false;
         }
         else
         {
             Debug.Log("Error: " + arg0 + " is not a valid hexadecimal value.");
+            badInput = true;
         }
     }
 }
