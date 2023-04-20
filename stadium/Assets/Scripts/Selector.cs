60819bbbcc84ea89feca3f6ba8dd07b493c83680
jtw0086 <jtw0086@auburn.edu>
2023-04-20 13:32:54 -0500

    Updated Selector

--- stadium/Assets/Scripts/Selector.cs
+++ stadium/Assets/Scripts/Selector.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class Selector : MonoBehaviour
 {
+    public bool glowing;
+    Material emissiveMaterial;
+    public Button sectionSelectorButton;
+    public InputField sectionSelectorInput;
 
     public List<Transform> GetAllChildren(Transform LEDs, bool recursive)
     {
@@ -21,52 +25,41 @@ public class Selector : MonoBehaviour
         return children;
     }
 
-    /*public GameObject[] GetAllLEDs()
+    void changeSectionLights(string[] sections)
     {
-        GameObject[] allLEDs;
-        string tag = "LED";
-        allLEDs = GameObject.FindGameObjectsWithTag(tag);
-
-        return allLEDs;
-    }
-
-    public List<GameObject> GetRow()
-    {
-
-    }*/
-
-    public List<Transform> GetSection(Transform LEDs)
-    {
-        List<Transform> children = GetAllChildren(transform, false);
-
-        foreach(Transform child in children)
+        glowing = !glowing;
+        foreach (string section in sections)
         {
-            GameObject[] allLeds;
-            string tag = "LED";
-            allLeds = GameObject.FindGameObjectsWithTag(tag);
-            foreach(GameObject LED in allLeds)
+            GameObject selectedSection = GameObject.Find(section);
+            Debug.Log(selectedSection);
+            Transform[] selectedSectionLights = selectedSection.GetComponentsInChildren<Transform>();
+            List<Transform> lightsList = new List<Transform>();
+            foreach (Transform t in selectedSectionLights)
             {
-                children.Add(LED.transform);
+                if (t.name.Contains("Sphere"))
+                {
+                    lightsList.Add(t.transform);
+                    emissiveMaterial = t.GetComponent<Renderer>().material;
+                    if (glowing)
+                    {
+                        emissiveMaterial.DisableKeyword("_EMISSION");
+                    }
+                    else
+                    {
+                        emissiveMaterial.EnableKeyword("_EMISSION");
+                    }
+                }
             }
         }
-        return children;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        List<Transform> children = GetAllChildren(transform, false);
-
-        foreach(Transform child in children)
-        {
-            GameObject[] allLeds;
-            string tag = "LED";
-            allLeds = GameObject.FindGameObjectsWithTag(tag);
-            foreach(GameObject LED in allLeds)
-            {
-                Debug.Log(LED.name);
-            }
-        }
+        glowing = true;
+        string[] sections = new string[]{"LEDs (1)", "LEDs"};
+        sectionSelectorButton.onClick.AddListener(delegate{changeSectionLights(sections);});
+        //changeSectionLights(section);
     }
 
     // Update is called once per frame
