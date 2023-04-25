e4a028e63cef8e362fbb298f452c372bfc61a2c4
1kzpro <kazymizam@mail.ru>
2023-04-25 17:58:34 -0500

    fix music volume slider and cleanup UI

--- stadium/Assets/Scripts/UI Control/Selector_Controller.cs
+++ stadium/Assets/Scripts/UI Control/Selector_Controller.cs
@@ -30,18 +30,18 @@ public class Selector_Controller : MonoBehaviour
     void toggleTopDeck()
     {
         TopDeck.SetActive(!TopDeck.activeSelf);
-        Debug.Log("Top");
+        // Debug.Log("Top");
     }
 
     void toggleUpperDeck()
     {
         UpperDeck.SetActive(!UpperDeck.activeSelf);
-        Debug.Log("Upper");
+        // Debug.Log("Upper");
     }
 
     void toggleLowerDeck()
     {
         LowerDeck.SetActive(!LowerDeck.activeSelf);
-        Debug.Log("Lower");
+        // Debug.Log("Lower");
     }
 }
